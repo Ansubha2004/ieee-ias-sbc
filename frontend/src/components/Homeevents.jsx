b83cf8ded142375291439a18026a1cfe4071551b
Ansubha Dhar <ansubhadhar2012@gmail.com>
2025-08-05 14:34:25 +0530

    home events update for ai app making event

--- frontend/src/components/Homeevents.jsx
+++ frontend/src/components/Homeevents.jsx
@@ -1,21 +1,21 @@
 import React from "react";
-import elevate from "../assets/upcomingevents/elevate.png";
+import event from "../assets/upcomingevents/aiappmakingevent.jpeg";
 import Button from "../components/Button.jsx";
 import Events from "../data/events.json";
 
 function Homeevents() {
   return (
-    <div className="w-full flex sm:flex-row flex-col-reverse justify-around">
+    <div className="w-full flex md:flex-row flex-col-reverse sm:gap-10 gap-0 justify-around">
       <div
         id="recents"
-        className="w-full sm:w-4/7 flex flex-col items-center sm:items-start justify-between rounded-[10px] "
+        className="w-full md:w-[45%] flex flex-col  items-center sm:items-start justify-between rounded-[10px] "
       >
         <br className="sm:hidden" />
         <p className="subheading  text-[1.5rem] mb-[24px]">
           Recent Highlights
         </p>
         <div className="flex flex-col w-full items-start gap-7 ">
-          {Events.map((event, index) => (
+          {Events.slice(1).map((event, index) => (
             <div key={index}>
               <a className="text-lime-800 font-[700] hover:underline" href={event.link}>{event.name}</a>
               <div className="paratext  w-full flex text-[0.8rem] gap-1">
@@ -32,13 +32,13 @@ function Homeevents() {
       </div>
       <div
         id="upcoming"
-        className="w-full sm:w-2/7 flex flex-col items-center rounded-[15px]  p-[1.5rem] bg-zinc-200 h-auto"
+        className="w-full md:w-[45%] flex flex-col items-center rounded-[15px]  p-[1.5rem] bg-zinc-200 h-auto"
       >
         <div className="font-[700] mb-2 text-[1.5rem] sm:text-[1.2rem]">
           Upcoming Event
         </div>
-        <img src={elevate} className="w-full sm:aspect-[4/5] lg:aspect-square" />
-        <Button themecss="btn3 mt-5" Content="View Details" />
+        <img src={event} className="w-full aspect-[16/11]" />
+        <Button themecss="btn3 mt-5" Content="Registration is Live" />
       </div>
     </div>
   );
